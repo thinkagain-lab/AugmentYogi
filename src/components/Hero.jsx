15320f83c6ac38054a9e75ca740709e29148684d
Prashnik <prashnikdas.tal@gmail.com>
2024-05-10 20:07:19 +0530

    button fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
             className="relative inline-flex items-center justify-center p-0.5 mb-2 mt-4 me-2 overflow-hidden text-sm font-medium  rounded-lg group bg-gradient-to-br from-customColor-brand-blue to-customColor-brand-magenta group-hover:from-customColor-brand-blue group-hover:to-pink-500 hover:text-white text-white focus:ring-4 focus:outline-none focus:bg-customColor-brand-magenta"
           >
 
-            <span className="relative text-base px-20 py-4 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            <span className="relative text-sm md:text-base px-20 py-4 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">
               Get Your Customized Filter
             </span>
           </a>
